c2e92b60ad2969c02f284f8cd8d7cb28df1cfab2
basstian-ai <stian.prestegaard@gmail.com>
2025-08-14 12:00:21 +0200

    Delete https:/github.com/basstian-ai/simple-pim-1754492683911 directory

--- https:/github.com/basstian-ai/simple-pim-1754492683911/https:/github.com/basstian-ai/simple-pim-1754492683911/src/components/VariantMatrixEditor.tsx
+++
@@ -1,9 +0,0 @@
-// VariantMatrixEditor component for editing variant attributes in a grid format.
-import React from 'react';
-
-const VariantMatrixEditor = () => {
-  // Component logic for handling variant attributes
-  return <div>Variant Matrix Editor</div>;
-};
-
-export default VariantMatrixEditor;
